d77dc5260d77bc5f54a89a559297e727a4068838
Ólafur Jóhannsson <olafurj@Moon.in.is>
2015-12-22 23:56:24 +0000

    readme

---
+++ README.md
@@ -0,0 +1,31 @@
+<h1>HTML TO PDF</h1>
+
+<p>
+Yet another HTML to PDF converter. This is a package i made to solve specific HTML conversion problems i had in one of my projects.
+</p>
+
+<p>
+The gist of it is that there are two methods
+<ul>
+<li>createFromHtml (html, pdfName, callback)</li>
+<li>createFromTemplateData (html, htmlData, pdfName, callback)</li>
+</ul>
+</p>
+
+<p>
+Both of these methods are what make up this package, the callback is a function that takes in two parameters, <strong>callback (error, success)</strong> which do what they imply, if success then no error, if not success then the error field is populated with the information needed to correctify.
+</p>
+
+<h2>Usage for <small>createFromHtml</small></h2>
+<p>
+The former method <i>createFromHtml(html, pdfName, callback)</i> takes in HTML, the name of the PDF to be created and the previously explained callback
+<code>
+htmltopdf.createFromHtml("&lthtml&gt&lth1&gthtml&lt/h1&gt&lt/html&gt", "pdfName.pdf", function (err, success) { ... });
+</code>
+</p>
+
+<h2>Usage for <small>createFromTemplateData</small></h2>
+<p>
+The latter method <i>createFromTemplateData(html, htmlData, pdfName, callback)</i> uses templates to create the PDF. You send in the template HTML, such as <blockquote>&gthtml&lt{{data}}&gt/html&lt</blockquote> and it's data <blockquote>{{'data':'test'}}</blockquote> <strong>
+</p>
+
--- src/main.js
+++ src/main.js
@@ -1,31 +1,31 @@
 var htmltopdf = require('./htmltopdf'),
 	fs = require('fs');
 
-
 if (process.argv.length > 2) {
 	var pdfName = process.argv[2];
 	var html = process.argv[3];
 
 	if (pdfName.indexOf('.pdf') > 0) {
 		var htmlData = process.argv[4];
-		
-		console.log('pdfName', pdfName);
-		console.log('html', html);
-		console.log('htmlData', htmlData);
-		
+				
 		if (!!htmlData) {
-			htmltopdf.createFromTemplateData(html, htmlData, function (err, success) {
+			htmltopdf.createFromTemplateData(html, htmlData, pdfName, function (err, success) {
 				if (success) {
 					console.log('Success creating ' + pdfName);
 				}
 				else {
-					console.log(err, success);
+					console.log('Could not create PDF', err);
 				}
 			});
 		}
 		else {
 			htmltopdf.createFromHtml(html, pdfName, function (err, success) {
-				console.log('Success creating ' + pdfName);
+				if (success) {
+					console.log('Success creating ' + pdfName);	
+				}
+				else {
+					console.log('Could not create PDF', err);
+				}
 			});
 		}
 	}
@@ -45,5 +45,5 @@ function printUsage() {
 	console.log('Send in string HTML and PDF name!');
 	console.log('Usage:');
 	console.log('Creating from HTML: node src/main test.pdf <html><h1>test</h1></html>');
-	console.log('Creating from HTML template: node src/main test.pdf <html><h1>{{test}}</h1></html> {test:123}');
+	console.log('Creating from HTML template: node src/main test.pdf "<html><h1>{{test}}</h1></html>" "{\"test\":123}"');
 }
