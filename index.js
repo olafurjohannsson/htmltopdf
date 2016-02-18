46d206e2044dc620015c9ceaadd3978ef7b7d49c
Ólafur <olafurjoh@ru.is>
2016-02-18 14:37:55 +0000

    fix formatting

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var phantom = require('phantom'),
-	handlebars = require('handlebars'),
-	fs = require('fs');
+    handlebars = require('handlebars'),
+    fs = require('fs');
 
 
 
 // Function ctor
 var PDF = function () {
-	this.isWindows = process.platform === 'win32';
+    this.isWindows = process.platform === 'win32';
 };
 
 
@@ -16,82 +16,82 @@ var PDF = function () {
 
 PDF.prototype._render = function (html, pdfName, fn) {
 
-	if (!html) {
-		throw new Error('Empty HTML string!');	
-	}
-	
-	if (!pdfName) {
-		throw new Error('Empty PDF name!');
-	}
+    if (!html) {
+        throw new Error('Empty HTML string!');
+    }
 
-	function render(ph, pdfName) {
-		
-		// Try to render PDF from HTML
-		try {	
-			// Create PhantomJS page
-			return ph.createPage(function (page) {
-				
-				// A4 as our papersize
-				page.set('paperSize', {
-					format: 'A4'
-				});
-				
+    if (!pdfName) {
+        throw new Error('Empty PDF name!');
+    }
+
+    function render(ph, pdfName) {
+
+        // Try to render PDF from HTML
+        try {
+            // Create PhantomJS page
+            return ph.createPage(function (page) {
+
+                // A4 as our papersize
+                page.set('paperSize', {
+                    format: 'A4'
+                });
+
                 // Set content in PhantomJS page
                 page.set('content', html, function () {
-                    
+
                     // render page
                     return page.render(pdfName, function () {
-                        
+
                         // Exit PhantomJS
                         ph.exit();
-                        
+
                         // If callback
                         if (!!fn)
                             return fn(null, true);
                     });
                 });
-			});
-		}
-		catch (e) {
-			ph.exit();
-			return fn(e, false);
-		}
-        
-	}
+            });
+        }
+        catch (e) {
+            ph.exit();
+            return fn(e, false);
+        }
 
-	/*
+    }
+
+    /*
 		Render our PDF from HTML
 	*/
-	if (this.isWindows) {
-		phantom.create(function (ph) { render(ph, pdfName); }, { dnodeOpts: { weak: false } });
-	}
-	else {
-		phantom.create(function (ph) { render(ph, pdfName); });
-	}
+    if (this.isWindows) {
+        phantom.create(function (ph) { render(ph, pdfName); }, { dnodeOpts: { weak: false } });
+    }
+    else {
+        phantom.create(function (ph) { render(ph, pdfName); });
+    }
 };
 
 // Create from HTML template
 PDF.prototype._create = function (htmlTemplateData, htmlData, pdfName, fn) {
 
-	// Compile our Handlebars template
-	var template = null;
+    // Compile our Handlebars template
+    var template = null;
     try {
         template = handlebars.compile(htmlTemplateData);
-    } 
+    }
     catch (e) {
         return fn('Could not compile HTML template data', false);
-    } 
-	
-	// If string
-	if (!(htmlData instanceof Object)) {
-		try {
-			htmlData = JSON.parse(htmlData);
-		}
-		catch (e) {
-			return fn('HTML is not valid JSON', false);
-		}
-	}
-	
+    }
+
+    // If string
+    if (!(htmlData instanceof Object)) {
+        try {
+            htmlData = JSON.parse(htmlData);
+        }
+        catch (e) {
+            return fn('HTML is not valid JSON', false);
+        }
+    }
+
     // Generate HTML string from the template
     var html = null;
     try {
@@ -100,9 +100,9 @@ PDF.prototype._create = function (htmlTemplateData, htmlData, pdfName, fn) {
     catch (e) {
         return fn('Could not inject HTML data into template', false);
     }
-	
-	// Render it to PDF
-	this._render(html, pdfName, fn);
+
+    // Render it to PDF
+    this._render(html, pdfName, fn);
 };
 
 
@@ -117,7 +117,7 @@ PDF.prototype._create = function (htmlTemplateData, htmlData, pdfName, fn) {
 	 @fn:				Callback which is called on error/success/fail
 */
 PDF.prototype.createFromTemplateData = function (htmlTemplateData, data, pdfName, fn) {
-	this._create(htmlTemplateData, data, pdfName, fn);
+    this._create(htmlTemplateData, data, pdfName, fn);
 };
 
 /* Create PDF from straight HTML
@@ -126,7 +126,7 @@ PDF.prototype.createFromTemplateData = function (htmlTemplateData, data, pdfName
 	 @fn:	   Callback which is called on error/success/fail
 */
 PDF.prototype.createFromHtml = function (html, pdfName, fn) {
-	this._render(html, pdfName, fn);
+    this._render(html, pdfName, fn);
 };
 
 // export our function
