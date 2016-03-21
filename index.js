7e49c1a22002d29b0ae2b4aae23bf6d102cf8863
unknown <oj@venuepoint.net>
2016-03-21 08:47:49 +0000

    add callback handler that takes care of rendering

--- index.js
+++ index.js
@@ -11,44 +11,49 @@ var PDF = function () {
 
 
 /*
-	*** Private interface ***
+    *** Private interface ***
 */
 
 PDF.prototype._render = function (html, pdfName, fn) {
 
     if (!html) {
-        throw new Error('Empty HTML string!');
+        throw new Error('Empty HTML string!');  
     }
-
+    
     if (!pdfName) {
         throw new Error('Empty PDF name!');
     }
 
     function render(ph, pdfName) {
-
+        
         // Try to render PDF from HTML
-        try {
+        try {   
             // Create PhantomJS page
             return ph.createPage(function (page) {
-
+                
                 // A4 as our papersize
                 page.set('paperSize', {
                     format: 'A4'
                 });
-
-                // Set content in PhantomJS page
-                page.set('content', html, function () {
-
+                
+                page.set('onLoadFinished', function () {
+                                        
                     // render page
                     return page.render(pdfName, function () {
-
+                        
                         // Exit PhantomJS
                         ph.exit();
-
+                        
                         // If callback
                         if (!!fn)
                             return fn(null, true);
                     });
+                });
+                
+                
+                // Set content in PhantomJS page
+                page.set('content', html, function () {
+                    
                 });
             });
         }
@@ -56,12 +61,12 @@ PDF.prototype._render = function (html, pdfName, fn) {
             ph.exit();
             return fn(e, false);
         }
-
+        
     }
 
     /*
-		Render our PDF from HTML
-	*/
+        Render our PDF from HTML
+    */
     if (this.isWindows) {
         phantom.create(function (ph) { render(ph, pdfName); }, { dnodeOpts: { weak: false } });
     }
@@ -77,11 +82,11 @@ PDF.prototype._create = function (htmlTemplateData, htmlData, pdfName, fn) {
     var template = null;
     try {
         template = handlebars.compile(htmlTemplateData);
-    }
+    } 
     catch (e) {
         return fn('Could not compile HTML template data', false);
-    }
-
+    } 
+    
     // If string
     if (!(htmlData instanceof Object)) {
         try {
@@ -91,7 +96,7 @@ PDF.prototype._create = function (htmlTemplateData, htmlData, pdfName, fn) {
             return fn('HTML is not valid JSON', false);
         }
     }
-
+    
     // Generate HTML string from the template
     var html = null;
     try {
@@ -100,30 +105,30 @@ PDF.prototype._create = function (htmlTemplateData, htmlData, pdfName, fn) {
     catch (e) {
         return fn('Could not inject HTML data into template', false);
     }
-
+    
     // Render it to PDF
     this._render(html, pdfName, fn);
 };
 
 
 /*
-	*** Public Interface ***
+    *** Public Interface ***
 */
 
 /* Create PDF from template and data
-	 @htmlTemplateData: The handlebars raw HTML template data
-	 @data: 			The data to feed to handlebars template
-	 @pdfName:		    The name of the outputted PDF
-	 @fn:				Callback which is called on error/success/fail
+     @htmlTemplateData: The handlebars raw HTML template data
+     @data:             The data to feed to handlebars template
+     @pdfName:          The name of the outputted PDF
+     @fn:               Callback which is called on error/success/fail
 */
 PDF.prototype.createFromTemplateData = function (htmlTemplateData, data, pdfName, fn) {
     this._create(htmlTemplateData, data, pdfName, fn);
 };
 
 /* Create PDF from straight HTML
-	 @html:	   HTML data with populated data 		
-	 @pdfName: The name of the outputted PDF
-	 @fn:	   Callback which is called on error/success/fail
+     @html:    HTML data with populated data        
+     @pdfName: The name of the outputted PDF
+     @fn:      Callback which is called on error/success/fail
 */
 PDF.prototype.createFromHtml = function (html, pdfName, fn) {
     this._render(html, pdfName, fn);
